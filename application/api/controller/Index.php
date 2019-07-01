6a0b147d60db8682899b06316703519005817244
FZJ <yudwcom@163.com>
2019-07-01 10:32:27 +0800

    去掉首页装修

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -47,17 +47,12 @@ class Index extends ApiBase
         $this->ajaxReturn(['status' => 0 , 'msg'=>'获取成功','data'=>$data]);
     }
 
-    /***
-     * 首页ID
-     */
+   
     public function page(){
         // $user_id = $this->get_user_id();
-        // if(!$user_id){
-        //     $this->ajaxReturn(['status' => -1 , 'msg'=>'用户不存在','data'=>'']);
-        // }
-        $ewei = Db::name('diy_ewei_shop')->where(['status' => 1])->find();
+        
 
-        $this->ajaxReturn(['status' => 1 , 'msg'=>'获取首页成功！','data'=>$ewei['id']]);
+        $this->ajaxReturn(['status' => 1 , 'msg'=>'获取首页成功！','data'=>'']);
     }
 
     
