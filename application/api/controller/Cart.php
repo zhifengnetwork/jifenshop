d5df77aa356ace420d4a835b4ed67b8b5493cddb
李柏成 <729127678@qq.com>
2019-07-02 12:19:52 +0800

    加入购物车接口

--- application/api/controller/Cart.php
+++ application/api/controller/Cart.php
@@ -44,11 +44,103 @@ class Cart extends ApiBase
 
         $this->ajaxReturn(['status' => 1 , 'msg'=>'成功','data'=>$num]);
     }
+    /**
+     * new 2019.7.2
+     * 加入 | 修改 购物车
+     *
+     */
+    public function addCart()
+    {
 
+        $user_id = $this->get_user_id();
+        if(!$user_id){
+            $this->ajaxReturn(['status' => -1 , 'msg'=>'用户不存在','data'=>'']);
+        }
+        // input('sku_id/d',0)
+
+        $sku_id       = Request::instance()->param("sku_id", 0, 'intval');
+        $cart_number  = Request::instance()->param("cart_number", 1, 'intval');
+        $act = Request::instance()->param('act');
+
+        if( !$sku_id || !$cart_number ){
+            $this->ajaxReturn(['status' => -2 , 'msg'=>'该商品不存在！','data'=>'']);
+        }
+
+        $sku_res = Db::name('goods_sku')->where('sku_id', $sku_id)->field('price,groupon_price,inventory,frozen_stock,goods_id')->find();
+
+        if (empty($sku_res)) {
+            $this->ajaxReturn(['status' => -2 , 'msg'=>'该商品不存在！','data'=>'']);
+        }
+
+        if ($cart_number > ($sku_res['inventory']-$sku_res['frozen_stock'])) {
+            $this->ajaxReturn(['status' => -2 , 'msg'=>'该商品库存不足！','data'=>'']);
+        }
+
+        $goods = Db::table('goods')->where('goods_id',$sku_res['goods_id'])->field('single_number,most_buy_number')->find();
+
+        if( $cart_number > $goods['single_number'] ){
+            $this->ajaxReturn(['status' => -2 , 'msg'=>"超过单次购买数量！同类商品单次只能购买{$goods['single_number']}个",'data'=>'']);
+        }
+
+        $order_goods_num = Db::table('order_goods')->alias('og')
+            ->join('order o','o.order_id=og.order_id')
+            ->where('o.order_status','neq',3)
+            ->where('og.goods_id',$sku_res['goods_id'])
+            ->where('og.user_id',$user_id)
+            ->sum('og.goods_num');
+
+        $num =  $cart_number + $order_goods_num;
+        if( $num > $goods['most_buy_number'] ){
+            $this->ajaxReturn(['status' => -2 , 'msg'=>'超过最多购买量！','data'=>'']);
+        }
+
+        $cart_where = array();
+        $cart_where['user_id'] = $user_id;
+        $cart_where['goods_id'] = $sku_res['goods_id'];
+
+        $act_where = [];
+        if($act){
+            $act_where['sku_id'] = ['not in',$sku_id];
+        }
+
+        $cart_goods_num = Db::table('cart')->where($cart_where)->where($act_where)->sum('goods_num');
+
+        $num = $cart_number + $cart_goods_num;
+        if( $num > $goods['single_number'] ){
+            $this->ajaxReturn(['status' => -2 , 'msg'=>"超过单次购买数量！同类商品单次只能购买{$goods['single_number']}个",'data'=>'']);
+        }
+        if( $num > $goods['most_buy_number'] ){
+            $this->ajaxReturn(['status' => -2 , 'msg'=>'超过最多购买量！','data'=>'']);
+        }
+
+        $cartData = array();
+        $goods_res = Db::name('goods')->where('goods_id',$sku_res['goods_id'])->field('goods_name,price,original_price')->find();
+        $cartData['goods_id'] = $sku_res['goods_id'];
+        $cartData['selected'] = 0;
+        $cartData['goods_name'] = $goods_res['goods_name'];
+        $cartData['sku_id'] = $sku_id;
+        $cartData['user_id'] = $user_id;
+        $cartData['market_price'] = $goods_res['original_price'];
+        $cartData['goods_price'] = $sku_res['price'];
+        $cartData['member_goods_price'] = $sku_res['price'];
+        $cartData['subtotal_price'] = $cart_number * $sku_res['price'];
+        $cartData['goods_num'] = $cart_number;
+        $cartData['add_time'] = time();
+        $sku_attr = action('Goods/get_sku_str', $sku_id);
+        $cartData['spec_key_name'] = $sku_attr;
+        $cart_id = Db::table('cart')->insertGetId($cartData);
+        $cart_id = intval($cart_id);
+
+        if($cart_id) {
+            $this->ajaxReturn(['status' => 1 , 'msg'=>'成功！','data'=>$cart_id]);
+        } else {
+            $this->ajaxReturn(['status' => -2 , 'msg'=>'系统异常！','data'=>'']);
+        }
+    }
     /**
      * 加入 | 修改 购物车
      */
-    public function addCart()
+    public function addCart2()
     {   
 
         $user_id = $this->get_user_id();
