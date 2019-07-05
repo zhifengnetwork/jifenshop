ed1e5637f21d3f852343ac8af35825a5a3478b7a
FZJ <yudwcom@163.com>
2019-07-05 12:07:49 +0800

    微信登录

--- application/api/controller/Login.php
+++ application/api/controller/Login.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: MyPC
- * Date: 2019/4/22
- * Time: 17:53
- */
-
 namespace app\api\controller;
 
 use think\Db;
@@ -13,19 +6,211 @@ use think\Loader;
 use think\Request;
 use think\Session;
 use think\captcha\Captcha;
-use app\common\util\jwt\JWT;
 
-class Login extends \think\Controller
+
+class Login extends ApiBase
 {
-    public function index () {
-        redirect('login/login')->send();
-        exit;
+
+    /**
+     * 获取code 的 url
+     */
+    public function get_code_url() {
+        
+        $baseUrl = I('baseUrl');
+        if(!$baseUrl){
+            $this->ajaxReturn(['status' => -1 , 'msg'=>'当前地址参数baseUrl为空','data'=>'']);
+        }
+
+        $appid = M('config')->where(['name'=>'appid'])->value('value');
+        $appsecret = M('config')->where(['name'=>'appsecret'])->value('value');
+        if(!$appid || !$appsecret){
+            $this->ajaxReturn(['status' => -1 , 'msg'=>'后台参数appid或appsecret配置为空','data'=>'']);
+        }
+    
+        $baseUrl = urlencode($baseUrl);
+
+        $url = $this->__CreateOauthUrlForCode($baseUrl,$appid,$appsecret); // 获取 code地址
+
+        $this->ajaxReturn(['status' => 1 , 'msg'=>'获取成功','data'=>$url]);
     }
 
+     /**
+     * 凭 code 登录
+     */
+    public function login_by_code() {
+        
+        $code = I('code');
+        if(!$code){
+            $this->ajaxReturn(['status' => -1 , 'msg'=>'参数code为空','data'=>'']);
+        }
+
+        $appid = M('config')->where(['name'=>'appid'])->value('value');
+        $appsecret = M('config')->where(['name'=>'appsecret'])->value('value');
+        if(!$appid || !$appsecret){
+            $this->ajaxReturn(['status' => -1 , 'msg'=>'后台参数appid或appsecret配置为空','data'=>'']);
+        }
+    
+        $data = $this->getOpenidFromMp($code,$appid,$appsecret);//获取网页授权access_token和用户openid
+        if(isset($data['errcode'])){
+            $this->ajaxReturn(['status' => -1 , 'msg'=>$data['errmsg'],'data'=>'']);
+        }
+
+        write_log("openid:".$data['openid']);
+
+        $data2 = $this->GetUserInfo($data['access_token'],$data['openid']);//获取微信用户信息
+        $data['nickname'] = empty($data2['nickname']) ? '微信用户' : trim($data2['nickname']);
+        $data['sex'] = $data2['sex'];
+        $data['head_pic'] = $data2['headimgurl']; 
+        $data['oauth_child'] = 'mp';
+        $data['oauth'] = 'weixin';
+        if(isset($data2['unionid'])){
+            $data['unionid'] = $data2['unionid'];
+        }
+       
+        //判断是否注册
+      
+        $field = 'id,openid,avatar';
+        $userinfo = M('member')->where(['openid'=>$data['openid']])->field($field)->find();
+        if(!$userinfo){
+            $newdata = array(
+                'openid' => $data['openid'],
+                'nickname' => $data['nickname'],
+                'createtime' => time(),
+                'avatar' => $data['head_pic']
+            );
+            M('member')->insert($newdata);
+
+            //再次查找
+            $userinfo = M('member')->where(['openid'=>$data['openid']])->field($field)->find();
+        }
+
+        //创建token
+        if(!$userinfo['id']){
+            $this->ajaxReturn(['status' => -1 , 'msg'=> '注册或登录出错' ,'data'=>'']);
+        }
+
+        $userinfo['token'] = $this->create_token($userinfo['id']);
+
+        $this->ajaxReturn(['status' => 1 , 'msg'=>'登录成功，你很棒棒','data'=>$userinfo]);
+    }
+
+    // {
+    //     "status": 1,
+    //     "msg": "获取成功",
+    //     "data": {
+    //         "access_token": "23_BW9BvJkD074_W9enPO35TYficTwdo-7v-1hmLJHNva51O9kSAsWShi4muBkvcFjmWonjX0z4XHms5Vh4iEG4G4uHoa7VDsWLEluOVrEpf1g",
+    //         "expires_in": 7200,
+    //         "refresh_token": "23_ygEN-F2IwC1zdZI0yRZbhaNmkIBVjy5xJPKIC5cbw0l-rXS0iQJZ8-FDqx0GuUaypuDLaEEjLLGnJnzqmKtGDJ3IeN45kWJqfEpAJee6844",
+    //         "openid": "oj0ty1rLtNAnkVXfW7LTyGQcdvZY",
+    //         "scope": "snsapi_userinfo",
+    //         "nickname": "精",
+    //         "sex": 1,
+    //         "head_pic": "http://thirdwx.qlogo.cn/mmopen/vi_32/eOb0Z9PVwFlt7Zm25XljEbN4GFqgGzN9DOoc07qGMwzdlZZQnia5OUFnTsxCiceiaH18NgI8Q8WhibGov7lq4859SQ/132",
+    //         "oauth_child": "mp",
+    //         "oauth": "weixin"
+    //     }
+    // }
 
 
+    /**
+     *
+     * 通过access_token openid 从工作平台获取UserInfo      
+     * @return openid
+     */
+    public function GetUserInfo($access_token,$openid)
+    {         
+        // 获取用户 信息
+        $url = $this->__CreateOauthUrlForUserinfo($access_token,$openid);
+        $ch = curl_init();//初始化curl        
+        curl_setopt($ch, CURLOPT_TIMEOUT, 300);//设置超时
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,FALSE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);         
+        $res = curl_exec($ch);//运行curl，结果以jason形式返回            
+        $data = json_decode($res,true);            
+        curl_close($ch);
+      
+        return $data;
+    }
 
+    /**
+     *
+     * 构造获取拉取用户信息(需scope为 snsapi_userinfo)的url地址     
+     * @return 请求的url
+     */
+    private function __CreateOauthUrlForUserinfo($access_token,$openid)
+    {
+        $urlObj["access_token"] = $access_token;
+        $urlObj["openid"] = $openid;
+        $urlObj["lang"] = 'zh_CN';        
+        $bizString = $this->ToUrlParams($urlObj);
+        return "https://api.weixin.qq.com/sns/userinfo?".$bizString;                    
+    }    
+    
 
+    /**
+     *
+     * 通过code从工作平台获取openid机器access_token
+     * @param string $code 微信跳转回来带上的code
+     *
+     * @return openid
+     */
+    public function GetOpenidFromMp($code,$appid,$appsecret)
+    {
+        //通过code获取网页授权access_token 和 openid 。网页授权access_token是一次性的，而基础支持的access_token的是有时间限制的：7200s。
+    	//1、微信网页授权是通过OAuth2.0机制实现的，在用户授权给公众号后，公众号可以获取到一个网页授权特有的接口调用凭证（网页授权access_token），通过网页授权access_token可以进行授权后接口调用，如获取用户基本信息；
+    	//2、其他微信接口，需要通过基础支持中的“获取access_token”接口来获取到的普通access_token调用。
+        $url = $this->__CreateOauthUrlForOpenid($code,$appid,$appsecret);       
+        $ch = curl_init();//初始化curl        
+        curl_setopt($ch, CURLOPT_TIMEOUT, 300);//设置超时
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,FALSE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);         
+        $res = curl_exec($ch);//运行curl，结果以jason形式返回            
+        $data = json_decode($res,true);         
+        curl_close($ch);
+        return $data;
+    }
+
+       /**
+     *
+     * 构造获取open和access_toke的url地址
+     * @param string $code，微信跳转带回的code
+     *
+     * @return 请求的url
+     */
+    private function __CreateOauthUrlForOpenid($code,$appid,$appsecret)
+    {
+        $urlObj["appid"] = $appid;
+        $urlObj["secret"] = $appsecret;
+        $urlObj["code"] = $code;
+        $urlObj["grant_type"] = "authorization_code";
+        $bizString = $this->ToUrlParams($urlObj);
+        return "https://api.weixin.qq.com/sns/oauth2/access_token?".$bizString;
+    }
+
+    /**
+     *
+     * 构造获取code的url连接
+     * @param string $redirectUrl 微信服务器回跳的url，需要url编码
+     *
+     * @return 返回构造好的url
+     */
+    private function __CreateOauthUrlForCode($redirectUrl,$appid,$appsecret)
+    {
+        $urlObj["appid"] = $appid;
+        $urlObj["redirect_uri"] = "$redirectUrl";
+        $urlObj["response_type"] = "code";
+        //$urlObj["scope"] = "snsapi_base";
+        $urlObj["scope"] = "snsapi_userinfo";
+        $urlObj["state"] = "STATE"."#wechat_redirect";
+        $bizString = $this->ToUrlParams($urlObj);
+        return "https://open.weixin.qq.com/connect/oauth2/authorize?".$bizString;
+    }
 
     /**
      * 登录接口
@@ -33,10 +218,6 @@ class Login extends \think\Controller
     public function login()
     {
 
-        header('Access-Control-Allow-Origin:*');
-        header('Access-Control-Allow-Headers:*');
-        header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
-        header('Content-Type:application/json; charset=utf-8');
 
         $mobile    = input('mobile');
         $password1 = input('password');
@@ -56,81 +237,29 @@ class Login extends \think\Controller
         //重写
         $data['token'] = $this->create_token($data['user_id']);
         
-        exit(json_encode(['status' => 0 , 'msg'=>'登录成功','data'=>$data],JSON_UNESCAPED_UNICODE));
 
+        $this->ajaxReturn(['status' => 1 , 'msg'=>'获取成功','data'=>$data]);
     }
 
-    /**
-     * 生成token
+     /**
+     *
+     * 拼接签名字符串
+     * @param array $urlObj
+     *
+     * @return 返回已经拼接好的字符串
      */
-    private function create_token($user_id){
-        $time = time();
-        $payload = array(
-            "iss"=> "DC",
-            "iat"=> $time ,
-            "exp"=> $time + 36000 ,
-            "user_id"=> $user_id
-        );
-        $key = 'zhelishimiyao';
-        $token = JWT::encode($payload, $key, $alg = 'HS256', $keyId = null, $head = null);
-        return $token;
+    private function ToUrlParams($urlObj)
+    {
+        $buff = "";
+        foreach ($urlObj as $k => $v)
+        {
+            if($k != "sign"){
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+
+        $buff = trim($buff, "&");
+        return $buff;
     }
 
-
-
-
-
-//    public function login () {
-//        if (Request::instance()->isPost()) {
-//            $username = input('username');
-//            $password = input('password');
-//            // 实例化验证器
-//            $validate = Loader::validate('Login');
-//            // 验证数据
-//            $data = ['username' => $username, 'password' => $password];
-//            // 验证
-//            $code = input('captcha');
-//            $str = session('captcha_id');
-//            $captcha = new \think\captcha\Captcha();
-//            if (!$captcha->check($code,$str)){
-//                return json(['code'=>0,'msg'=>'验证码错误']);
-//            }
-//            if (!$validate->check($data)) {
-//                return $this->error($validate->getError());
-//            }
-//            $where['username'] = $username;
-//            $where['status']   = 1;
-//            $user_info = Db::table('mg_user')->where($where)->find();
-//            if ($user_info && $user_info['password'] === minishop_md5($password, $user_info['salt'])) {
-//                $session['uid']     = $user_info['mgid'];
-//                $session['user_name'] = $user_info['username'];
-//                // 记录用户登录信息
-//                Session::set('admin_user_auth', $session);
-//                return json(['code'=>1,'msg'=>'登录成功']);
-//            }
-//            return json(['code'=>0,'msg'=>'密码错误！']);
-//        }
-//    }
-
-
-
-//    /*
-//     *  获取验证码
-//      */
-//    public function loginCaptcha () {
-//        $str  = time().uniqid();
-//        Session::set('captcha_id', $str);
-//        $captcha = new Captcha();
-//        return $captcha->entry($str);
-//    }
-//
-//    /*
-//     * 退出登录
-//     */
-//    public function login_out()
-//    {
-//        session('admin_user_auth', null);
-//        session('ALL_MENU_LIST', null);
-//        return json(['code'=>1,'msg'=>'请登录','data'=>'']);
-//    }
 }
