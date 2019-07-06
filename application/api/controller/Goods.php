4a760b22fbd6229746871ef23df50ed1dda235ec
FZJ <yudwcom@163.com>
2019-07-06 17:25:29 +0800

    is_del

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -248,7 +248,7 @@ class Goods extends ApiBase
 
         $goodsinfo = Db::table('goods')->field('g.content,g.goods_name,g.price,g.original_price,g.is_own')->alias('g')
                     ->join('goods_img b','g.goods_id=b.goods_id')
-                    ->where('g.is_show',1)->where('g. is_del',0)
+                    ->where(['g.is_show'=>1,'g.is_del'=>0])
                     ->find($goods_id);
         if (empty($goodsinfo)) {
             $this->ajaxReturn(['status' => -2 , 'msg'=>'商品不存在！']);
