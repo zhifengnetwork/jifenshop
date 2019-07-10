eced95fcfb588a90856aac569ab134864a7c5ce6
李柏成 <729127678@qq.com>
2019-07-10 16:11:29 +0800

    bug

--- application/api/controller/Order.php
+++ application/api/controller/Order.php
@@ -1182,7 +1182,7 @@ class Order extends ApiBase
             'order_status' => 1,
             'pay_status'   => 1,
             'pay_type'     => 1,
-            'balance'      =>$amount,
+            'user_money'      =>$amount,
             'pay_time'     => time(),
         ];
         $reult = Db::table('order')->where(['order_id' => $order_id])->update($update);
