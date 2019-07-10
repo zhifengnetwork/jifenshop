efff06c45c443c6928f4c0c1ab3eefd9215e8a34
李柏成 <729127678@qq.com>
2019-07-10 18:20:57 +0800

    1

--- application/common.php
+++ application/common.php
@@ -684,7 +684,9 @@ function update_pay_status($order_sn,$ext=array())
     write_log('common line 684   '.$order_sn);
     $data=json_encode($ext);
     write_log('common line 686   '.$data);
-    $amount=sprintf("%.2f",$data['total_fee']/100);write_log('common line 687   ');
+    write_log('common line 687   ');
+    write_log('common line 688   '.$data['total_fee'].'  order_sn   '.$data["out_trade_no"]);
+    $amount=sprintf("%.2f",$data['total_fee']/100);
     $order = Db::table('order')->where(['order_sn' => '$data["out_trade_no"]'])->field('order_id,groupon_id,user_id,pay_status')->find();
     $sql=Db::table('order')->getLastSql();
     write_log('common line 689   sql===  '.$sql);
