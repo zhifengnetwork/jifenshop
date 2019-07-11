909925df14472066759818863cdc489199ac047b
FZJ <yudwcom@163.com>
2019-07-11 09:58:10 +0800

    修改token有效时间7天

--- application/api/controller/ApiBase.php
+++ application/api/controller/ApiBase.php
@@ -3,13 +3,12 @@
  * 继承
  */
 namespace app\api\controller;
-use app\common\util\jwt\JWT;
-use think\Db;
-use think\Controller;
+
 use app\common\model\Config;
-use think\Request;
-use think\Session;
+use app\common\util\jwt\JWT;
 use app\common\util\Redis;
+use think\Controller;
+use think\Db;
 
 class ApiBase extends Controller
 {
@@ -17,7 +16,8 @@ class ApiBase extends Controller
     protected $user_name;
     protected $is_bing_mobile;
 
-    public function _initialize () {
+    public function _initialize()
+    {
         header("Access-Control-Allow-Origin:*");
         header("Access-Control-Allow-Headers:*");
         header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
@@ -27,10 +27,11 @@ class ApiBase extends Controller
 
     }
 
-    private  static $redis = null;
+    private static $redis = null;
     /*获取redis对象*/
-    protected function getRedis(){
-        if(!self::$redis instanceof Redis){
+    protected function getRedis()
+    {
+        if (!self::$redis instanceof Redis) {
             self::$redis = new Redis(Config('cache.redis'));
         }
         return self::$redis;
@@ -39,7 +40,8 @@ class ApiBase extends Controller
     /*
      *  开放有可能不需登录controller
      */
-    private function freeLoginController () {
+    private function freeLoginController()
+    {
         $controller = [
             'Shop' => 'shop',
 //            'User' => 'user',
@@ -47,24 +49,26 @@ class ApiBase extends Controller
         return $controller;
     }
 
-    public function ajaxReturn($data){
+    public function ajaxReturn($data)
+    {
         header('Access-Control-Allow-Origin:*');
         header('Access-Control-Allow-Headers:*');
         header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
         header('Content-Type:application/json; charset=utf-8');
-        exit(str_replace("\\/", "/",json_encode($data,JSON_UNESCAPED_UNICODE)));
+        exit(str_replace("\\/", "/", json_encode($data, JSON_UNESCAPED_UNICODE)));
     }
 
     /**
      * 生成token
      */
-    public function create_token($user_id){
+    public function create_token($user_id)
+    {
         $time = time();
         $payload = array(
-            "iss"=> "DC",
-            "iat"=> $time ,
-            "exp"=> $time + 36000 ,
-            "user_id"=> $user_id
+            "iss" => "JIFENSHOP",
+            "iat" => $time,
+            "exp" => $time + 604800, //7天过期
+            "user_id" => $user_id,
         );
         $key = 'zhelishimiyao';
         $token = JWT::encode($payload, $key, $alg = 'HS256', $keyId = null, $head = null);
@@ -74,50 +78,50 @@ class ApiBase extends Controller
     /**
      * 解密token
      */
-    public function decode_token($token){
-        $key     = 'zhelishimiyao';
-        $payload = json_decode(json_encode(JWT::decode($token, $key, ['HS256'])),true);
+    public function decode_token($token)
+    {
+        $key = 'zhelishimiyao';
+        $payload = json_decode(json_encode(JWT::decode($token, $key, ['HS256'])), true);
         return $payload;
     }
 
     /**
-    *
-    *接收头信息
-    **/
+     *
+     *接收头信息
+     **/
     public function em_getallheaders()
     {
-       foreach ($_SERVER as $name => $value)
-       {
-           if (substr($name, 0, 5) == 'HTTP_')
-           {
-               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
-           }
-       }
-       return $headers;
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
     }
 
     /**
      * 获取user_id
      */
-    public function get_user_id($url = null){
+    public function get_user_id($url = null)
+    {
         $headers = $this->em_getallheaders();
-    
+
         $token = isset($headers['Token']) ? $headers['Token'] : input('token');
-        if(strlen($token) < 10){
+        if (strlen($token) < 10) {
             $token = input('token');
         }
 
         $user_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJEQyIsImlhdCI6MTU1OTYzOTg3MCwiZXhwIjoxNTU5Njc1ODcwLCJ1c2VyX2lkIjo3Nn0.YUQ3hG3TiXzz_5U594tLOyGYUzAwfzgDD8jZFY9n1WA';
 
-        if($user_token == $token){
+        if ($user_token == $token) {
             return 51;
-        }else{
-            if(!$token || $token == null || $token == 'null' || strlen($token) <= 10 ){
+        } else {
+            if (!$token || $token == null || $token == 'null' || strlen($token) <= 10) {
                 //401
                 header('HTTP/1.1 401 Unauthorized');
                 header('Status: 401 Unauthorized');
-                
-                $this->ajaxReturn(['status' => -1 , 'msg'=>'token不存在('.$url.')'.$token.microtime(),'data'=>null]);
+
+                $this->ajaxReturn(['status' => -1, 'msg' => 'token不存在(' . $url . ')' . $token . microtime(), 'data' => null]);
             }
 
             $tks = explode('.', $token);
@@ -125,65 +129,65 @@ class ApiBase extends Controller
                 //401
                 header('HTTP/1.1 401 Unauthorized');
                 header('Status: 401 Unauthorized');
-                $this->ajaxReturn(['status' => -1 , 'msg'=>'token格式错误:'.$token,'data'=>null]);
+                $this->ajaxReturn(['status' => -1, 'msg' => 'token格式错误:' . $token, 'data' => null]);
             }
 
             $res = $this->decode_token($token);
-            if(!$res){
+            if (!$res) {
                 //401
                 header('HTTP/1.1 401 Unauthorized');
                 header('Status: 401 Unauthorized');
-                $this->ajaxReturn(['status' => -1 , 'msg'=>'无效token'.json_encode($res),'data'=>null]);
+                $this->ajaxReturn(['status' => -1, 'msg' => '无效token' . json_encode($res), 'data' => null]);
             }
-           
-            if(!isset($res['iat']) ){
+
+            if (!isset($res['iat'])) {
                 //401
                 header('HTTP/1.1 401 Unauthorized');
                 header('Status: 401 Unauthorized');
-                $this->ajaxReturn(['status' => -1 , 'msg'=>'token错误:iat不存在'.json_encode($res),'data'=>null]);
+                $this->ajaxReturn(['status' => -1, 'msg' => 'token错误:iat不存在' . json_encode($res), 'data' => null]);
             }
-            if(!isset($res['exp']) ){
+            if (!isset($res['exp'])) {
                 //401
                 header('HTTP/1.1 401 Unauthorized');
                 header('Status: 401 Unauthorized');
-                $this->ajaxReturn(['status' => -1 , 'msg'=>'token错误exp不存在'.json_encode($res),'data'=>null]);
+                $this->ajaxReturn(['status' => -1, 'msg' => 'token错误exp不存在' . json_encode($res), 'data' => null]);
             }
-            if($res['iat']>$res['exp']){
-                 //401
-                 header('HTTP/1.1 401 Unauthorized');
-                 header('Status: 401 Unauthorized');
-                $this->ajaxReturn(['status' => -1 , 'msg'=>'token已过期'.json_encode($res),'data'=>null]);
-            }
-            if(!isset($res['user_id']) ){
+            if ($res['iat'] > $res['exp']) {
                 //401
                 header('HTTP/1.1 401 Unauthorized');
                 header('Status: 401 Unauthorized');
-                $this->ajaxReturn(['status' => -1 , 'msg'=>'token出错'.json_encode($res),'data'=>null]);
+                $this->ajaxReturn(['status' => -1, 'msg' => 'token已过期' . json_encode($res), 'data' => null]);
+            }
+            if (!isset($res['user_id'])) {
+                //401
+                header('HTTP/1.1 401 Unauthorized');
+                header('Status: 401 Unauthorized');
+                $this->ajaxReturn(['status' => -1, 'msg' => 'token出错' . json_encode($res), 'data' => null]);
             }
             return $res['user_id'];
         }
     }
 
-    
     /**
      *  判断是否绑定手机号码
      */
-    protected function is_bing_mobile ($openid) {
+    protected function is_bing_mobile($openid)
+    {
 
-        $mobile = Db::table('member')->where('openid',$openid)->value('mobile');
-        if (empty($mobile)){
+        $mobile = Db::table('member')->where('openid', $openid)->value('mobile');
+        if (empty($mobile)) {
             return false;
-        }else{
+        } else {
             return true;
         }
 
     }
 
-
     /**
      * 空
      */
-    public function _empty(){
-        $this->ajaxReturn(['status' => -1 , 'msg'=>'接口不存在','data'=>null]);
+    public function _empty()
+    {
+        $this->ajaxReturn(['status' => -1, 'msg' => '接口不存在', 'data' => null]);
     }
 }
